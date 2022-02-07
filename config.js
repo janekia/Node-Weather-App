85c5d6e6712aa1e074edc07f31f5e5397e371d40
janekia <84926731+janekia@users.noreply.github.com>
2022-02-07 18:43:27 -0500

    my commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "0a6d24ec50c8f08d1522dad60a0c517f"
+    }
+}
+
+module.exports = constants;
